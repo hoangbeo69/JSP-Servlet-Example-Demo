781c13f058c5c424d271240614ddd19994a95a0f
hoangbeo69 <48564756+hoangbeo69@users.noreply.github.com>
2020-09-08 06:35:29 +0700

    add them model

--- src/main/java/com/jspservletjdbc/model/UserModel.java
+++ src/main/java/com/jspservletjdbc/model/UserModel.java
@@ -1,9 +1,26 @@
 package com.jspservletjdbc.model;
 
-public class UserModel {
+import java.sql.Timestamp;
+
+public class UserModel extends AbstractModel{
+    private long id;
     private String userName;
     private String fullName;
+    private String passWord;
+    private boolean status;
+    private long roleId;
+    private Timestamp createDate;
+    private Timestamp modifiedDate;
+    private String createdBy;
+    private String modifiedBy;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
     public String getFullName() {
         return fullName;
@@ -13,11 +30,27 @@ public class UserModel {
         this.fullName = fullName;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getPassWord() {
+        return passWord;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
     }
 }
