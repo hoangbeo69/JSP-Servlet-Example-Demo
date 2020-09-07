e8db2aed7c12ca517c2b5ac8694be8de7bf69c97
hoangbeo69 <48564756+hoangbeo69@users.noreply.github.com>
2020-09-08 06:54:47 +0700

    them cac model

---
+++ src/main/java/com/jspservletjdbc/model/CommentModel.java
@@ -0,0 +1,32 @@
+package com.jspservletjdbc.model;
+
+public class CommentModel extends AbstractModel {
+    private String content;
+    private Long userId;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getNewId() {
+        return newId;
+    }
+
+    public void setNewId(Long newId) {
+        this.newId = newId;
+    }
+
+    private Long newId;
+}
