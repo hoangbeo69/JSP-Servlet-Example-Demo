761a5f6799a8db43bc1685604194b32812a63528
hoangbeo69 <48564756+hoangbeo69@users.noreply.github.com>
2020-09-09 09:23:52 +0700

    thêm chỉnh sủa phần service và cách gọi cơ bản từ DAO

--- src/main/java/com/jspservletjdbc/service/impl/CategoryService.java
+++ src/main/java/com/jspservletjdbc/service/impl/CategoryService.java
@@ -1,12 +1,21 @@
 package com.jspservletjdbc.service.impl;
 
+import com.jspservletjdbc.dao.ICategoryDAO;
+import com.jspservletjdbc.dao.impl.CategoryDAO;
 import com.jspservletjdbc.model.CategoryModel;
 import com.jspservletjdbc.service.ICategoryService;
 
 import java.util.List;
 
 public class CategoryService implements ICategoryService {
+//      Gọi kiểu cơ bản
+    private ICategoryDAO categoryDAO;
+
+    public CategoryService(){
+        categoryDAO = new CategoryDAO();
+    }
+
     public List<CategoryModel> findAll(){
-        return  null;
+        return  categoryDAO.findAll();
     }
 }
