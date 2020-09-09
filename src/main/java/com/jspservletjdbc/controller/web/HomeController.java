c175b112280f25ed3d193c01e44c4cba767a6dca
hoangbeo69 <48564756+hoangbeo69@users.noreply.github.com>
2020-09-09 09:33:23 +0700

    demo sử dụng java servlet weld

--- src/main/java/com/jspservletjdbc/controller/web/HomeController.java
+++ src/main/java/com/jspservletjdbc/controller/web/HomeController.java
@@ -1,7 +1,9 @@
 package com.jspservletjdbc.controller.web;
 
 import com.jspservletjdbc.model.UserModel;
+import com.jspservletjdbc.service.ICategoryService;
 
+import javax.inject.Inject;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,11 +15,17 @@ import java.io.IOException;
 //@WebServlet(urlPatterns = {"/home","/login"})
 @WebServlet(urlPatterns = {"/home"})
 public class HomeController extends HttpServlet {
+    @Inject
+    private ICategoryService categoryService;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
+
 //        UserModel user = new UserModel();
 //        user.setFullName("Hell everyone");
 //        request.setAttribute("model",user); //đặt tên đối tượng này là model để các file JSP có thể gọi
+
+        request.setAttribute("categorys",categoryService.findAll());
         RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
         rd.forward(request,response);
     }
--- src/main/java/com/jspservletjdbc/service/impl/CategoryService.java
+++ src/main/java/com/jspservletjdbc/service/impl/CategoryService.java
@@ -5,15 +5,19 @@ import com.jspservletjdbc.dao.impl.CategoryDAO;
 import com.jspservletjdbc.model.CategoryModel;
 import com.jspservletjdbc.service.ICategoryService;
 
+import javax.inject.Inject;
 import java.util.List;
 
 public class CategoryService implements ICategoryService {
 //      Gọi kiểu cơ bản
+//    private ICategoryDAO categoryDAO;
+//
+//    public CategoryService(){
+//        categoryDAO = new CategoryDAO();
+//    }
+    //sử dụng Java Servlet Weld
+    @Inject
     private ICategoryDAO categoryDAO;
-
-    public CategoryService(){
-        categoryDAO = new CategoryDAO();
-    }
 
     public List<CategoryModel> findAll(){
         return  categoryDAO.findAll();
