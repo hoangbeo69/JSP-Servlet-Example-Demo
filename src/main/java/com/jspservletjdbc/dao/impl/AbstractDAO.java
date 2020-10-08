f00419e1d1c8a142108b3f603eeec9a0a70e2da3
hoangbeo69 <48564756+hoangbeo69@users.noreply.github.com>
2020-10-08 10:32:16 +0700

    thay đổi lại cơ sở dữ liệu

--- src/main/java/com/jspservletjdbc/dao/impl/AbstractDAO.java
+++ src/main/java/com/jspservletjdbc/dao/impl/AbstractDAO.java
@@ -12,7 +12,7 @@ public class AbstractDAO<T> implements GenericDAO<T> {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/jspservletjdbc_demo";
+            String url = "jdbc:mysql://localhost:3306/jspservletjdbc";
             String user = "root";
             String password = "123456";
             return DriverManager.getConnection(url, user, password);
